2ca514affa2d360830f8dbf2f00b4ce7d5396d12
Nathan Wright <nathan@brandalism.com>
2025-01-23 19:05:31 +0300

    update build config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,58 @@
-const path = require("path");
+// const path = require("path");
 
-/**
- * @type {import('vite').UserConfig}
- */
-const config = {
-  // ...
+// /**
+//  * @type {import('vite').UserConfig}
+//  */
+// const config = {
+//   // ...
+//   build: {
+//     target: "es2017",
+//     lib: {
+//       entry: path.resolve(__dirname, "./src/index.js"),
+//       name: "RestrictFocus",
+//       fileName: (format) => `restrict-focus.${format}.js`,
+//       output: {
+//         exports: "named",
+//       },
+//     },
+//     rollupOptions: {
+//       output: {
+//         exports: "named",
+//       },
+//     },
+//   },
+// };
+
+// export default config;
+
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
+
+export default defineConfig({
   build: {
-    target: "es2017",
     lib: {
-      entry: path.resolve(__dirname, "./src/index.js"),
-      name: "RestrictFocus",
-      fileName: (format) => `restrict-focus.${format}.js`,
-      output: {
-        exports: "named",
-      },
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "restrictFocus",
+      fileName: "restrictFocus",
+      formats: ["es", "umd"],
     },
     rollupOptions: {
+      // external: [], // specify external dependencies here if any
       output: {
-        exports: "named",
+        // Provide globals for UMD build if you have external dependencies
+        globals: {
+          // 'some-dependency': 'SomeDependency'
+        },
       },
     },
   },
-};
-
-export default config;
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  server: {
+    open: "/index.html", // Automatically open demo page
+  },
+});
